a1c4ca86a44b683cbb53392cf32f14be142f447e
Edward White <edwardwhite1@outlook.com>
2016-01-01 22:08:15 +0000

    fixed some ActionMailer Errors

--- config/environment.rb
+++ config/environment.rb
@@ -3,10 +3,11 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+# ActionMailer Settings
 ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port => '587',
-    :authentication => .plain,
+    :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
     :domain => 'heroku.com',
